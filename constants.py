a54a27e5cfad9b804c223a8b56dbd710a8b2e244
MonzerMahmoud <55148623+MonzerMahmoud@users.noreply.github.com>
2022-08-25 10:15:00 +0200

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,9 +1,9 @@
-BASE_URL = 'https://animetak.vip/'
-WATCH_URL = BASE_URL + 'watch_episodes/'
+BASE_URL = ''
+WATCH_URL = BASE_URL + ''
 
 def getDBZURL(episode):
-    return WATCH_URL + '%D8%A7%D9%86%D9%85%D9%8A-dragon-ball-z-%D8%A7%D9%84%D8%AD%D9%84%D9%82%D8%A9-{}-%D9%85%D8%AA%D8%B1%D8%AC%D9%85%D8%A9-%D8%A7%D9%88%D9%86-%D9%84%D8%A7%D9%8A%D9%86.html'.format(episode)
+    return WATCH_URL + ''.format(episode)
 
 def get_black_clover_url(episode):
     
-    return WATCH_URL + 'hunter-x-hunter-%D8%A7%D9%84%D8%AD%D9%84%D9%82%D8%A9-{}-%D9%85%D8%AA%D8%B1%D8%AC%D9%85%D8%A9.html'.format(episode)
+    return WATCH_URL + ''.format(episode)
